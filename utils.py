2884452cbb5578a2326c50ef2557c669886bf81b
gamethis-369 <gamethis.net@gmail.com>
2026-01-04 01:10:47 -0500

    Add function to plot data using matplotlib

---
+++ utils.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+def plot_data(x, y, title, xlabel, ylabel):
+    plt.figure()
+    plt.plot(x, y)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
